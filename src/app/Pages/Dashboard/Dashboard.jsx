b96b9d77bb5370ba2371501ccc3c0d7870ccf098
rishikesh1014 <rishikeshnarayan14@gmail.com>
2024-10-09 12:01:51 +0530

    Update Dashboard.jsx
    github desktop demo

--- src/app/Pages/Dashboard/Dashboard.jsx
+++ src/app/Pages/Dashboard/Dashboard.jsx
@@ -6,7 +6,7 @@ const Dashboard = () => {
     <div className="dashboard">
         <div className="dashboard_nav">
             <h1>CRM</h1>
-            
+            this is the dashboard page.
         </div>
     </div>
   )
